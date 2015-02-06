de3f9b995981c4dd53c78fa07ac43a8727d54238
kevin14 <kevin14@me.com>
2015-02-06 22:25:24 +0800

    完成init方法的链式调用

--- core/widget.js
+++ core/widget.js
@@ -1,46 +1,59 @@
-define(['base','FFF'], function(Base,FFF) {
+define(['base', 'FFF'], function(Base, FFF) {
+
+    function Widget() {
+        Base.apply(this, arguments);
+        initWidget.call(this);
+    }
+
+    /**
+     * Interface
+     * @return {[type]} [description]
+     */
+    Widget.prototype.initialize = function() {};
+    Widget.prototype.renderUI = function() {};
+    Widget.prototype.bindUI = function() {};
+    Widget.prototype.syncUI = function() {};
+    Widget.prototype.destructor = function() {};
+
+    /**
+     * 渲染方法
+     * @param {Object} opt 渲染的参数
+     * @return {Object} 对象本身
+     */
+    Widget.prototype.render = function(opt) {
+        this.renderUI();
+        this.bindUI();
+        this.syncUI();
+        return this;
+    }
+
+    Widget.prototype.destory = function() {
+        return this;
+    }
 
 
-	function Widget(){
-		Base.apply(this,arguments);
-	}
+    Widget.ATTRS = {
+        boundingBox: {
+            value: 'widget'
+        }
+    }
 
-	//interface must be realized
-	Widget.interface = ['initilize','renderUI','bindUI','syncUI'];
+    /**
+     * 从父类开始调用所有子类的initialize方法
+     * 这样initialize方法将成为所有控件的入口
+     */
+    function initWidget() {
+        var initializers = [];
+        var ctx = this;
+        do {
+            initializers.push(ctx.initialize);
+            ctx = ctx.super || {};
+        } while (ctx.constructor.prototype.hasOwnProperty('initialize'));
+        for (var i = initializers.length - 1; i >= 0; i--) {
+            initializers[i]();
+        };
+    }
 
-	Widget.prototype.interface = function(){
-    	return this;
-	}
-
-	Widget.prototype.render = function(){
-		console.log(123)
-    	return this;
-	}
-
-	Widget.prototype.destory = function(){
-    	return this;
-	}
-
-	Widget.prototype.initAttr = function(attr){
-		return this;
-	}
-
-	//attr getter
-	Widget.prototype.getAttr = function(attrName){
-		return this;
-	}
-
-	//attr setter
-	Widget.prototype.setAttr = function(obj){
-		return this;
-	}
-
-	Widget.ATTRS = {
-		boundingBox:{
-			value:'boundingBox'
-		}
-	}
-
-	return FFF.core.extend(Widget,Base);
+    return FFF.core.extend(Widget, Base);
 
 });
