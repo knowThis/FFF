0a2938f9a8c754ea524f93916c31fc8de05c5261
kevin14 <kevin14@me.com>
2015-02-10 20:13:53 +0800

    更改了evt的名字，并且新增了languagejs

--- core/base.js
+++ core/base.js
@@ -1,15 +1,15 @@
-define(['FFF','attribute','handler'], function(FFF,Attribute,Handler) {
+define(['FFF','attribute','eventEmitter'], function(FFF,Attribute,EventEmitter) {
 
     /**
      * FFF基础类,所有类都讲继承Base
      */
     function Base() {
         Attribute.apply(this, arguments);
-        Handler.apply(this, arguments);
+        EventEmitter.apply(this, arguments);
     }
 
     FFF.core.mix(Base.prototype, Attribute.prototype, false);
-    FFF.core.mix(Base.prototype, Handler.prototype, false);
+    FFF.core.mix(Base.prototype, EventEmitter.prototype, false);
 
 
     return Base;
