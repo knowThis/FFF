88d9a143eb45abd9bc83a228e7d2c77b2532bc3a
lb1064 <lb1064@qq.com>
2015-02-10 20:43:44 +0800

    加入测试代码

--- test/slider.js
+++ test/slider.js
@@ -1,6 +1,7 @@
 define(['FFF','widget'], function(FFF,widget) {
 
     function WidgetA() {
+        widget.apply(this, arguments);
     }
 
 
@@ -9,8 +10,10 @@ define(['FFF','widget'], function(FFF,widget) {
     };
     WidgetA.prototype.renderUI = function() {
         var me = this;
+        //TODO 此处需要任彬写的mc来处理
+        //或者改造handler中的event处理模式
         me.on('loadData',function(){
-            console.log(arguments);
+            console.log(new Date().getTime());
         })
     };
     WidgetA.prototype.bindUI = function() {
